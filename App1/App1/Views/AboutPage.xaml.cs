9e7ee09f9486baa0fc94e7af7da69518507c9115
Krzysztof Słowiak <Firensik@gmail.com>
2022-01-19 19:50:17 +0100

    Dodaj pliki projektów.

---
+++ App1/App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            
+        }
+        int count = 0;
+        private void ButtonClick_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            LabelCount.Text = $"You click{count} time(s)";
+        }
+        
+    }
+}
